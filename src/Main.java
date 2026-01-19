caac808ebf77a2516a68b725582492d44352f535
Бертан <254491@astanait.edu.kz>
2026-01-20 00:48:46 +0500

    assignment 3

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Menu menu = new MenuManager();
+        Scanner scanner = new Scanner(System.in);
+
+        menu.showMenu();
+        int choice = scanner.nextInt();
+
+        try {
+            menu.handleChoice(choice);
+        } catch (InvalidChoiceException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
